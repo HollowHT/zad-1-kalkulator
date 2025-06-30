90c169767d0b233975cd1153eda0eb1a4dfa67ba
Mateusz Przybyslawski <m.przybyslawski@vp.pl>
2025-06-30 02:04:19 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace fr_kalk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.eq = new System.Windows.Forms.Button();
+            this.plu = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.multi = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // one
+            // 
+            this.one.BackColor = System.Drawing.Color.White;
+            this.one.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.one.Location = new System.Drawing.Point(302, 167);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(63, 47);
+            this.one.TabIndex = 0;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = false;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.BackColor = System.Drawing.Color.White;
+            this.two.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.two.Location = new System.Drawing.Point(380, 167);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(63, 47);
+            this.two.TabIndex = 1;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = false;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // nine
+            // 
+            this.nine.BackColor = System.Drawing.Color.White;
+            this.nine.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.nine.Location = new System.Drawing.Point(461, 293);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(63, 47);
+            this.nine.TabIndex = 2;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = false;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // eight
+            // 
+            this.eight.BackColor = System.Drawing.Color.White;
+            this.eight.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.eight.Location = new System.Drawing.Point(380, 293);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(63, 47);
+            this.eight.TabIndex = 3;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = false;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // four
+            // 
+            this.four.BackColor = System.Drawing.Color.White;
+            this.four.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.four.Location = new System.Drawing.Point(302, 230);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(63, 47);
+            this.four.TabIndex = 4;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = false;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.BackColor = System.Drawing.Color.White;
+            this.five.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.five.Location = new System.Drawing.Point(380, 230);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(63, 47);
+            this.five.TabIndex = 5;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = false;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // three
+            // 
+            this.three.BackColor = System.Drawing.Color.White;
+            this.three.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.three.Location = new System.Drawing.Point(461, 167);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(63, 47);
+            this.three.TabIndex = 6;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = false;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.Color.White;
+            this.seven.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.seven.Location = new System.Drawing.Point(302, 293);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(63, 47);
+            this.seven.TabIndex = 7;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = false;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // six
+            // 
+            this.six.BackColor = System.Drawing.Color.White;
+            this.six.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.six.Location = new System.Drawing.Point(461, 230);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(63, 47);
+            this.six.TabIndex = 8;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = false;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // zero
+            // 
+            this.zero.BackColor = System.Drawing.Color.White;
+            this.zero.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.zero.Location = new System.Drawing.Point(302, 356);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(63, 47);
+            this.zero.TabIndex = 9;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = false;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // eq
+            // 
+            this.eq.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.eq.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.eq.Location = new System.Drawing.Point(461, 356);
+            this.eq.Name = "eq";
+            this.eq.Size = new System.Drawing.Size(63, 47);
+            this.eq.TabIndex = 10;
+            this.eq.Text = "=";
+            this.eq.UseVisualStyleBackColor = false;
+            this.eq.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // plu
+            // 
+            this.plu.BackColor = System.Drawing.Color.Lime;
+            this.plu.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.plu.Location = new System.Drawing.Point(557, 356);
+            this.plu.Name = "plu";
+            this.plu.Size = new System.Drawing.Size(63, 47);
+            this.plu.TabIndex = 11;
+            this.plu.Text = "+";
+            this.plu.UseVisualStyleBackColor = false;
+            this.plu.Click += new System.EventHandler(this.plu_Click);
+            // 
+            // min
+            // 
+            this.min.BackColor = System.Drawing.Color.Yellow;
+            this.min.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.min.Location = new System.Drawing.Point(557, 293);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(63, 47);
+            this.min.TabIndex = 12;
+            this.min.Text = "-";
+            this.min.UseVisualStyleBackColor = false;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // multi
+            // 
+            this.multi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.multi.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.multi.Location = new System.Drawing.Point(557, 230);
+            this.multi.Name = "multi";
+            this.multi.Size = new System.Drawing.Size(63, 47);
+            this.multi.TabIndex = 13;
+            this.multi.Text = "*";
+            this.multi.UseVisualStyleBackColor = false;
+            this.multi.Click += new System.EventHandler(this.multi_Click);
+            // 
+            // div
+            // 
+            this.div.BackColor = System.Drawing.Color.Red;
+            this.div.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.div.Location = new System.Drawing.Point(557, 167);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(63, 47);
+            this.div.TabIndex = 14;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = false;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(302, 107);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(318, 20);
+            this.txtDisplay.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.multi);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.plu);
+            this.Controls.Add(this.eq);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button eq;
+        private System.Windows.Forms.Button plu;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button multi;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.TextBox txtDisplay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fr_kalk
+{
+    public partial class Form1 : Form
+    {
+       
+        double startnum = 0;
+        bool newnum = false;
+        string operation = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            double endnum;
+            double wynik = 0;
+
+            if (double.TryParse(txtDisplay.Text, out endnum))
+            {
+                switch (operation)
+                {
+                    case "+":
+                        wynik = startnum + endnum;
+                        break;
+                    case "-":
+                        wynik = startnum - endnum;
+                        break;
+                    case "*":
+                        wynik = startnum * endnum;
+                        break;
+                    case "/":
+                        if (endnum != 0)
+                            wynik = startnum / endnum;
+                        else
+                        {
+                            MessageBox.Show("Nie dziel przez zero!");
+                            return;
+                        }
+                        break;
+                }
+
+                txtDisplay.Text = wynik.ToString();
+                newnum = true;
+            }
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if(newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+                
+            }
+            txtDisplay.Text += btn.Text;
+
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+               
+            }
+            txtDisplay.Text += btn.Text;
+
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+                
+            }
+            txtDisplay.Text += btn.Text;
+
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+                
+            }
+            txtDisplay.Text += btn.Text;
+
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+               
+            }
+
+            txtDisplay.Text += btn.Text;
+
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+                
+            }
+            txtDisplay.Text += btn.Text;
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+               
+            }
+            txtDisplay.Text += btn.Text;
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+                
+            }
+            txtDisplay.Text += btn.Text;
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+               
+            }
+            txtDisplay.Text += btn.Text;
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (newnum)
+            {
+                txtDisplay.Text = "";
+                newnum = false;
+
+                
+            }
+            txtDisplay.Text += btn.Text;
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (double.TryParse(txtDisplay.Text, out startnum))
+            {
+                operation = btn.Text;
+                newnum = true;
+            }
+        }
+
+        private void multi_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (double.TryParse(txtDisplay.Text, out startnum))
+            {
+                operation = btn.Text;
+                newnum = true;
+            }
+        }
+
+        private void min_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (double.TryParse(txtDisplay.Text, out startnum))
+            {
+                operation = btn.Text;
+                newnum = true;
+            }
+        }
+
+        private void plu_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (double.TryParse(txtDisplay.Text, out startnum))
+            {
+                operation = btn.Text;
+                newnum = true;
+            }
+        }
+    }
+}
